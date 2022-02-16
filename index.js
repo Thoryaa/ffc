de8c1ad10eb3ebb37677f1c8659b267580e2b6fc
Thoryaa <93941131+Thoryaa@users.noreply.github.com>
2022-02-16 08:59:54 +0300

    final

--- index.js
+++ index.js
@@ -7,28 +7,39 @@ app.get('/', (rer, res) => {
     res.sendFile(__dirname + '/index.html');
 
 });
-app.use(express.static('./assests'));
 app.get('/blog', (rer, res) => {
     res.sendFile(__dirname + '//view/blog.html');
 
 });
-app.use(express.static('./assests'));
 app.get('/contact', (rer, res) => {
     res.sendFile(__dirname + '/view/contact.html');
 
 });
-app.use(express.static('./assests'));
 app.get('/about', (rer, res) => {
     res.sendFile(__dirname + '/view/about.html');
 
 });
-app.use(express.static('./assests'));
 app.get('/login', (rer, res) => {
     res.sendFile(__dirname + '/view/login.html');
 
 });
-app.use(express.static('./assests'));
-app.get('/login', (rer, res) => {
+app.get('/services', (rer, res) => {
     res.sendFile(__dirname + '/view/services.html');
 
 });
+app.get('/admin?role=admin', (req, res) => {
+    if (req.url.includes("/admin?role=admin")) {
+        res.sendFile(__dirname + '/view/hi.html');
+    } else
+        res.sendFile(__dirname + '/view/error.html');
+
+
+
+});
+
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/view/error.html');
+    console.log(res.url);
+
+})
